8130d0a338b7b7db02efa7fe6782f30aa9da0d44
FarhanFardid <farhanfardid009@gmail.com>
2023-02-18 20:44:31 +0600

    result showing part working and calculation already completed

--- js/interactive.js
+++ js/interactive.js
@@ -59,3 +59,113 @@ document.getElementById('pentagon_card').addEventListener('mouseover', function(
 document.getElementById('ellipse_card').addEventListener('mouseover', function() {
     setBgColor('ellipse_card');
 });
+
+// area calculation function
+function areaTriRomPen(val1, val2){
+    const area = (0.5 * val1 * val2).toFixed(2);
+    return area;
+} 
+
+function areaRectPara(val1, val2){
+    const area = (val1 * val2).toFixed(2);
+    return area;
+}
+
+function areaEllipse(val1,val2){
+    const area = (3.14 * val1 * val2).toFixed(2);
+    return area;
+}
+
+function showArea(name, area){
+    const result =document.getElementById('list_order');
+    const li = document.createElement('li');
+          console.log("inside show area") ;
+            
+          const geo = name + '';
+          const res = area + ''
+    li.innerHTML =  geo +' '+ res +' '+ "(cm)";  
+    
+    result.appendChild(li);
+}
+// triangle area with input validation
+document.getElementById('triangle').addEventListener('click', function(){
+    const base = document.getElementById('triangle-base');
+    const baseNum = parseFloat(base.value);
+    const height = document.getElementById('triangle-height');
+    const heightNum = parseFloat(height.value);
+   
+    
+
+    if((typeof (baseNum) !== 'number' || typeof (heightNum) !== 'number') || (baseNum < 0 || heightNum< 0) || (base.value.length == '0' || height.value.length == '0') ) {
+
+        alert ("Please, Enter a valid positive number input type for all the input");
+        
+    }
+  
+    else if((!/^[0-9]+$/.test(baseNum)) || (!/^[0-9]+$/.test(heightNum)))
+    {
+        alert("Please, Enter Numbers only");
+    }
+    else{
+        const triArea = areaTriRomPen(baseNum, heightNum);
+        showArea('Triangle', triArea);
+        console.log(triArea) ;
+       
+    }
+});
+
+// rectangle area with input validation
+
+document.getElementById('rectangle').addEventListener('click', function(){
+    const width = document.getElementById('rectangle-width');
+    const widthNum = parseFloat(width.value);
+    const length = document.getElementById('rectangle-length');
+    const lengthNum = parseFloat(length.value);
+   
+    
+
+    if((typeof (widthNum) !== 'number' || typeof (lengthNum) !== 'number') || (widthNum < 0 || lengthNum < 0) || (width.value.length == '0' || length.value.length == '0') ) {
+
+        alert ("Please, Enter a valid positive number input type for all the input");
+        
+    }
+  
+    else if((!/^[0-9]+$/.test(widthNum)) || (!/^[0-9]+$/.test(lengthNum)))
+    {
+        alert("Please, Enter Numbers only");
+    }
+    else{
+        const rectArea = areaRectPara(widthNum, lengthNum);
+        showArea('Rectangle', rectArea);
+        console.log(rectArea) ;
+       
+    }
+});
+
+// parallelogram area
+document.getElementById('parallelogram').addEventListener('click', function(){
+    const parallelArea = areaRectPara(10, 12);
+    showArea('Parallelogram', parallelArea);
+    console.log(parallelArea);
+});
+
+// rhombus area
+document.getElementById('rhombus').addEventListener('click', function(){
+    const rhombusArea = areaTriRomPen(16, 8);
+    showArea('Rhombus', rhombusArea);
+    console.log(rhombusArea);
+});
+
+// pentagon area
+document.getElementById('pentagon').addEventListener('click', function(){
+    const pentagonArea = areaTriRomPen(6,10);
+    showArea('Pentagon', pentagonArea);
+    console.log(pentagonArea);
+});
+// ellipse area
+
+document.getElementById('ellipse').addEventListener('click', function(){
+    const ellipseArea = areaEllipse(10,4);
+    showArea('Ellipse', ellipseArea);
+    console.log(ellipseArea);
+});
